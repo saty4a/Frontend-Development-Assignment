fbfaae6729367e675b997ffe339d872eab73787d
saty4a <satyabratbiswal5413@gmail.com>
2024-01-08 04:31:25 +0530

    first commit

--- src/Components/navBar.jsx
+++ src/Components/navBar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import NavElements from "./NavElements";
+import NavElements from "./navElements";
 
 const NavBar = ({ setShowNavBar, navAnimation, setNavAnimation }) => {
   const ref = useRef();
